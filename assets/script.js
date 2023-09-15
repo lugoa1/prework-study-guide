1ec5a99a5debc03bec4a2450389078c5019847be
lugoa1 <wickedpissah@icloud.com>
2023-09-15 14:33:40 -0400

    <this is my javascript>

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+let topics = ["HTML","CSS","GIT","javaScript"]
+for (let y = 0; y < topics.length; y++){console.log(topics[y]);}
+
+
+let shapes = ["triangle", "square","pentagon", "circle"]
+for (let x= 0; x < shapes.length; x++){console.log(shapes[x]);}
+
