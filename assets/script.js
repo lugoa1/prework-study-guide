0051b5b45b1a9219c0342fd8f2ba38084f2dca13
lugoa1 <wickedpissah@icloud.com>
2023-09-15 17:18:15 -0400

    more javacode

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,27 @@
+
 let topics = ["HTML","CSS","GIT","javaScript"]
-for (let y = 0; y < topics.length; y++){console.log(topics[y]);}
+let randomTopic = topics [Math.floor(Math.random()*topics.length)];
+
+function listTopics() {
+for (let x = 0; x < topics.length; x++) {console.log(topics[x]);} }
 
 
-let shapes = ["triangle", "square","pentagon", "circle"]
-for (let x= 0; x < shapes.length; x++){console.log(shapes[x]);}
-
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+   }
+   
+   
+   console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
